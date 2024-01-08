2bb703d4dc99a9616f34a34753ac52167ade4f15
Djavlar <151103120+Djavlar@users.noreply.github.com>
2024-01-08 02:25:36 -0500

    Update index.js

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const steamUser = require('steam-user');
 const steamTotp = require('steam-totp');
 const keep_alive = require('./keep_alive.js')
 
-var username = '';
-var password = '';
-var shared_secret = '';
+var username = 'mrpulgonius';
+var password = '971216009';
+var shared_secret = 'TazSZPf6BTS9Fc++\/njl3AMdNRE=';
 
 var games = [570, 1172470, 730, 322330, 4000, 1905180, 2357570, 444090, 1840, 440, 550, 245170, 40800, 105600, 620, 1671210];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
